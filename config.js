29434ba91ffbcefdda1ace9ec5dc8b74d040633b
Jacky Tang <jackytck@gmail.com>
2018-11-30 16:38:11 +0800

    :wrench: update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const ALTI_KEY = '7MkQf8UggsPaadvrlKALspJWZejZAJOLHn3cnIy'
 export const ALTI_CALLBACK = 'https://jackytck.github.io/altizure-google-drive-integration/index.html'
 
-export const G_DEV_KEY = 'AIzaSyBV_Eo3xawQGDWyjbh_b3uryevkH_qPlL0'
-export const G_CLIENT_ID = '844127801372-chhhiuuoabd5mvt49k7hecb2mhd970mg.apps.googleusercontent.com'
-export const G_SCOPE = 'https://www.googleapis.com/auth/drive.photos.readonly'
+export const G_DEV_KEY = 'AIzaSyAeyIi9-LcFHT3cplvMkQl8q9-3Ur-iUu8'
+export const G_CLIENT_ID = '753960030086-5k9v19hregov1kjboto79qavpgfbjt4p.apps.googleusercontent.com'
+export const G_SCOPE = 'https://www.googleapis.com/auth/drive'
