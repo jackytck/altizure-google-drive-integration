64db8007f2539e343a3dd8826497b25b5e817eee
Jacky Tang <jackytck@gmail.com>
2018-12-04 15:40:29 +0800

    Updates

--- altizure-google-drive-integration.e31bb0bc.js
+++ altizure-google-drive-integration.e31bb0bc.js
@@ -387,7 +387,7 @@ function setupGoogle(pickerCallback) {
 
 
 function onApiLoad() {
-  gapi.load('auth2', onAuthApiLoad);
+  gapi.load('client:auth2', onAuthApiLoad);
   gapi.load('picker', onPickerApiLoad);
 }
 
@@ -484,7 +484,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57101" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60812" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
